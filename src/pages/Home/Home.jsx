da09486efa420ea21d9e1a575836e9c3c07b9616
sagarPakhrin <sagarlama826@gmail.com>
2023-08-06 09:03:39 +0545

    feat: Integrate api for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,49 +1,43 @@
-import Hero from "./Hero";
-import Women from "../../assets/images/category1.png";
-import Men from "../../assets/images/category2.png";
-import PregnantWomen from "../../assets/images/category3.png";
-import Child from "../../assets/images/category4.png";
-import NewArrials from "./NewArrivals";
-import LatestNews from "./LatestNews";
-import SubscriptionSection from "./Subscription";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Hero from './Hero';
+import LatestNews from './LatestNews';
+import NewArrials from './NewArrivals';
+import SubscriptionSection from './Subscription';
 
 const CategoryCard = ({ category }) => {
   return (
-    <div className="relative flex justify-center w-full md:w-1/2 lg:w-1/4 px-1 my-4">
-      <img src={category.image} alt="category" className="w-full" />
-      <div className="absolute bg-gray-300 px-14 py-4 -bottom-6">
-        {category.category}
+    <div className='relative flex justify-center w-full md:w-1/2 lg:w-1/4 px-1 my-4'>
+      <img src={category.image} alt='category' className='w-full' />
+      <div className='absolute bg-gray-300 px-14 py-4 -bottom-6'>
+        {category.name}
       </div>
     </div>
   );
 };
 
 const Home = () => {
-  const categories = [
-    {
-      category: "For Women",
-      image: Women,
-    },
-    {
-      category: "For Men",
-      image: Men,
-    },
-    {
-      category: "For Pregnant Women",
-      image: PregnantWomen,
-    },
-    {
-      category: "For Children",
-      image: Child,
-    },
-  ];
+  const [data, setData] = useState();
+
+  useEffect(function () {
+    axios
+      .get('http://localhost:3001/api/v1/home/')
+      .then(function (response) {
+        return response.data;
+      })
+      .then((data) => {
+        setData(data.data);
+      });
+  }, []);
+
+  const categories = data?.categories ?? [];
 
   return (
     <>
       <Hero />
       <section>
-        <div className="container mx-auto py-36 px-4 lg:px-0">
-          <div className="flex flex-wrap justify-between">
+        <div className='container mx-auto py-36 px-4 lg:px-0'>
+          <div className='flex flex-wrap justify-between'>
             {categories.map((category) => {
               return <CategoryCard category={category} />;
             })}
