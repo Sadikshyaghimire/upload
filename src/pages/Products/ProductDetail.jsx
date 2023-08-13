f8d979deb2f3b23e1d8a38cdbf189c261a951b23
sagarPakhrin <sagarlama826@gmail.com>
2023-08-13 08:15:55 +0545

    wip

--- src/pages/Products/ProductDetail.jsx
+++ src/pages/Products/ProductDetail.jsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from '../../api/axios';
@@ -30,6 +31,29 @@ const ProductDetail = () => {
           </div>
           <div className='w-1/2 pl-4'>
             <h1 className='text-3xl font-bold'>{product.name}</h1>
+            <p className='mt-2 text-gray-600'>$ {product.price}</p>
+            <p className='mt-5 text-stone-600'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+              animi delectus quos ex veritatis, saepe repellat consequuntur quas
+              placeat incidunt.
+            </p>
+            <div className='my-5 py-4 border-y border-gray-400'>
+              <div className='flex gap-4'>
+                <input
+                  type='number'
+                  className='border border-gray-500 w-20 py-2'
+                />
+                <div className='border border-gray-400 py-3 px-5 flex gap-5'>
+                  <button>
+                    <ChevronLeftIcon className='w-4 h-4 text-gray-400' />
+                  </button>
+                  <button>
+                    <ChevronRightIcon className='w-4 h-4 text-gray-400' />
+                  </button>
+                </div>
+                <button>Add to cart</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
